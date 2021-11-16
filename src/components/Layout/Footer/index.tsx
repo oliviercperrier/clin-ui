995d4edf1b15c7bd0660fed2160548c2d2072a38
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2021-11-16 16:24:21 -0500

    CLIN-449: Add main layout and route for variant entity

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import { Col, Layout, Row, Typography } from "antd";
 import intl from "react-intl-universal";
 import get from "lodash/get";
+import IconKit from "react-icons-kit";
+import {
+  ic_email,
+  ic_launch,
+  ic_location_on,
+  ic_phone,
+} from "react-icons-kit/md";
 
 import styles from "./index.module.scss";
 
@@ -35,8 +42,11 @@ const Footer = () => (
               <li>
                 <a href="#">{intl.get("footer.navigation.primary.faq")}</a>
               </li>
-              <li>
-                <a href="#">{intl.get("footer.navigation.primary.link")}</a>
+              <li className={styles.externalLink}>
+                <a href="#">
+                  {intl.get("footer.navigation.primary.link")}
+                  <IconKit icon={ic_launch} size={16} />
+                </a>
               </li>
             </ul>
           </nav>
@@ -47,9 +57,18 @@ const Footer = () => (
           </Text>
           <nav>
             <ul>
-              <li>{intl.get("footer.navigation.primary.phone")}</li>
-              <li>{intl.get("footer.navigation.primary.email")}</li>
-              <li>{intl.get("footer.navigation.primary.address")}</li>
+              <li>
+                <IconKit icon={ic_phone} size={16} />
+                {intl.get("footer.navigation.primary.phone")}
+              </li>
+              <li>
+                <IconKit icon={ic_email} size={16} />
+                {intl.get("footer.navigation.primary.email")}
+              </li>
+              <li>
+                <IconKit icon={ic_location_on} size={16} />
+                {intl.get("footer.navigation.primary.address")}
+              </li>
             </ul>
           </nav>
         </Col>
@@ -83,10 +102,14 @@ const Footer = () => (
               <a href="#">{intl.get("footer.navigation.secondary.about")}</a>
             </li>
             <li>
-              <a href={ZEPLIN_URL} target="_blank">Zeppelin</a>
+              <a href={ZEPLIN_URL} target="_blank">
+                Zeppelin
+              </a>
             </li>
             <li>
-              <a href={FHIR_CONSOLE_URL} target="_blank">Fhir</a>
+              <a href={FHIR_CONSOLE_URL} target="_blank">
+                Fhir
+              </a>
             </li>
           </ul>
         </nav>
