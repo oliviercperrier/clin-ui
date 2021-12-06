2bb88b5033744668f0e765e31cdb8015877f6911
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2021-12-06 15:20:30 -0500

    CLIN-511: Fix table styling and itemcount

--- src/views/screens/variant/Entity/PatientPanel/index.tsx
+++ src/views/screens/variant/Entity/PatientPanel/index.tsx
@@ -183,7 +183,6 @@ const PatientPanel = ({ hash, className = '' }: OwnProps) => {
               className: styles.patientPagination,
               hideOnSinglePage: true,
               onChange: (page, pageSize) => {
-                console.log(pageSize)
                 if (currentPage !== page || currentPageSize !== pageSize) {
                   setCurrentPage(page);
                   setCurrentPageSize(pageSize || DEFAULT_PAGE_SIZE);
