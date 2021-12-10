422db76b48bdfa7a8b7f6c8be6c6977715ca865a
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2021-12-10 14:49:45 -0500

    CLIN-495: Add igv first test

--- src/components/icons/CanonicalIcon.tsx
+++ src/components/icons/CanonicalIcon.tsx
@@ -9,7 +9,7 @@ const CanonicalIcon = ({ className = '', width = '18', height = '18', fill }: Ic
     height={height}
     viewBox="0 0 18 18"
     xmlns="http://www.w3.org/2000/svg"
-    fill="currentCurrent"
+    fill="currentColor"
   >
     <path
       d="M0 9C0 4.02944 4.02944 0 9 0C13.9706 0 18 4.02944 18 9C18 13.9706 13.9706 18 9 18C4.02944 18 0 13.9706 0 9Z"
