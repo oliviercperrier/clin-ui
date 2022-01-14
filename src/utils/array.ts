499efe5cf24f48205d0a111fe9001074f987eff1
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2022-01-14 13:08:58 -0500

    CLIN-703: fix

--- src/utils/array.ts
+++
@@ -1,5 +0,0 @@
-export const arrayPrepend = (value: any, array: any[]) => {
-  var newArray = array.slice();
-  newArray.unshift(value);
-  return newArray;
-};
--- src/views/screens/variant/utils.ts
+++ src/views/screens/variant/utils.ts
@@ -1,12 +1,9 @@
 import { TSqonContent } from '@ferlab/ui/core/data/sqon/types';
-import { arrayPrepend } from 'utils/array';
 
-export const prependPatientSqon = (content: TSqonContent, patientId: string) => {
-  return arrayPrepend(
-    {
-      content: { field: 'donors.patient_id', value: [patientId] },
-      op: 'in',
-    },
-    content,
-  );
-};
+export const prependPatientSqon = (content: TSqonContent, patientId: string) => [
+  {
+    content: { field: 'donors.patient_id', value: [patientId] },
+    op: 'in',
+  },
+  ...content,
+];
