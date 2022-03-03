697210a6d6d45bee60ec6b03a5bccd41ce9e3e60
evans-g-crsj <eg.crsj@gmail.com>
2022-03-03 10:27:06 -0500

    feat: CLIN-836 add more robustness and incorporate code review comments

--- README.md
+++ README.md
@@ -39,6 +39,11 @@ Instead, it will copy all the configuration files and the transitive dependencie
 
 You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
 
+### `npm run add-translation`
+If you need to add a translation, you can do so with this script. When running it simply input the key of the translation piped with the French translation itself piped with the English one. For example:   `a.b.c | bonjour le monde! | hello world!`. It will write these values to the corresponding translations (json) files.
+
+**Note: this script is not conceived to be robust, so make sure to verify that the outputs are as expected **
+
 ## Learn More
 
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
--- scripts/add-translation.js
+++ scripts/add-translation.js
@@ -10,18 +10,31 @@ const addTranslationsThenSort = (answers) =>
     const pathToTranslations = path.join(`src/locales/`, `${locale}.json`);
     const sfileContent = fs.readFileSync(pathToTranslations, 'utf8');
     const ofileContent = JSON.parse(sfileContent);
+    if (Object.keys(ofileContent).includes(answers.key)) {
+      console.log(
+        `The key:${answers.key} already exists in ${pathToTranslations} and will be overwritten!`,
+      );
+    }
     const newFileContent = { [answers.key]: answers[locale], ...ofileContent };
     const sortedFileContent = sortObject(newFileContent);
     fs.writeFileSync(pathToTranslations, JSON.stringify(sortedFileContent, null, 2));
   });
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
+const nOfExpectedInputs = 3;
+
 rl.question('Enter translation in the following format (key | fr | en) :', (input) => {
-  const [key, fr, en] = input.split('|');
+  const split = input.split('|');
+  if (split.length !== nOfExpectedInputs || split.some((x) => !x)) {
+    console.log('Input Invalid');
+    rl.close();
+    return;
+  }
+  const [key, fr, en] = split.map((x) => x?.trim());
   addTranslationsThenSort({
     key,
     fr,
