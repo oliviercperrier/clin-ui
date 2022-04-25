590ef9782957be8f3304fc42e25cceb48ff2d6b0
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2022-04-25 17:45:34 -0400

    fix: CLIN-979 fix styling entity variant

--- src/views/screens/variant/Entity/SummaryPanel/AcmgCriteria.tsx
+++ src/views/screens/variant/Entity/SummaryPanel/AcmgCriteria.tsx
@@ -1,65 +1,71 @@
-import React from 'react';
 import { Spin, Table, Typography } from 'antd';
 import intl from 'react-intl-universal';
-import {
-    VariantEntity,
-  } from 'graphql/variants/models';
+import { VariantEntity } from 'graphql/variants/models';
 import CollapsePanel from 'components/containers/collapse';
+import NoData from '../NoData';
 
 const { Title } = Typography;
 
 const columns = [
-    {
-        title: () => intl.get('screen.variantDetails.summaryTab.acmgCriteriaTable.criteriaColumn'),
-        dataIndex: 'name',
-        className: ``,
-        width: '18%',
-    },
-    {
-        title: () => intl.get('screen.variantDetails.summaryTab.acmgCriteriaTable.explanationColumn'),
-        dataIndex: 'user_explain',
-        className: ``,
-    },
-]
+  {
+    title: () => intl.get('screen.variantDetails.summaryTab.acmgCriteriaTable.criteriaColumn'),
+    dataIndex: 'name',
+    className: ``,
+    width: '18%',
+  },
+  {
+    title: () => intl.get('screen.variantDetails.summaryTab.acmgCriteriaTable.explanationColumn'),
+    dataIndex: 'user_explain',
+    className: ``,
+  },
+];
 
 const formatData = (data: VariantEntity | null) => {
-    if (!data) return [];
+  if (!data) return [];
 
-    return data.varsome?.acmg.classifications?.hits.edges.map((c) => {
-        const node = c.node;
-        return {
-            key: node.name,
-            name: node.name,
-            criteria: node.met_criteria,
-            user_explain: node.user_explain
-        }
-    })
-}
+  return data.varsome?.acmg.classifications?.hits.edges.map((c) => {
+    const node = c.node;
+    return {
+      key: node.name,
+      name: node.name,
+      criteria: node.met_criteria,
+      user_explain: node.user_explain,
+    };
+  });
+};
 
 type Props = {
-    isOpen?: boolean,
-    data: {
-        loading: boolean;
-        variantData: VariantEntity | null;
-    };
-}
+  isOpen?: boolean;
+  data: {
+    loading: boolean;
+    variantData: VariantEntity | null;
+  };
+};
 
 const Header = () => (
-    <Title level={4}>{intl.get('screen.variantDetails.summaryTab.acmgCriteriaTitle')}</Title>
-)
-
-const ACMGCriteria = ({data, isOpen = true}: Props) => (
-    <CollapsePanel header={<Header />}>
-        <Spin spinning={data.loading}>
-            <Table
-                bordered={true}
-                dataSource={formatData(data.variantData) || []}
-                columns={columns}
-                pagination={false}
-                size="small"
-            />
-        </Spin>
-    </CollapsePanel>
+  <Title level={4}>{intl.get('screen.variantDetails.summaryTab.acmgCriteriaTitle')}</Title>
 );
+
+const ACMGCriteria = ({ data, isOpen = true }: Props) => {
+  const formattedDate = formatData(data.variantData) || [];
+
+  return (
+    <CollapsePanel header={<Header />}>
+      <Spin spinning={data.loading}>
+        {formattedDate.length > 0 ? (
+          <Table
+            bordered={true}
+            dataSource={formatData(data.variantData) || []}
+            columns={columns}
+            pagination={false}
+            size="small"
+          />
+        ) : (
+          <NoData />
+        )}
+      </Spin>
+    </CollapsePanel>
+  );
+};
 
 export default ACMGCriteria;
